b5bec45aac08f67fa475c4b7a8f57bb8e8035861
Theo Lebegue <theo.lebegue1@gmail.com>
2021-08-16 10:06:26 +0200

    ft/ functions for the total

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tipPercentage = document.getElementsByClassName("tipPercentage");
 let activeClassName = "active";
+let bill = document.getElementById("bill");
 
 function activeState(items, activeName) {
   for (let i = 0; i < items.length; i++) {
@@ -14,4 +15,22 @@ for (let i = 0; i < tipPercentage.length; i++) {
     activeState(tipPercentage, activeClassName);
     e.target.classList.add(activeClassName);
   });
+}
+
+function calculateTipPercentage() {
+  function retrieveTipPercentage() {
+    for (let i = 0; i < tipPercentage.length; i++) {
+      if (tipPercentage[i].classList.contains("active")) {
+        return tipPercentage[i].value;
+      }
+    }
+  }
+  let chosenTip = retrieveTipPercentage();
+  let result2 = document.getElementById("result2");
+
+  result2.innerText = (chosenTip / 100) * bill.value;
+}
+
+for (let i = 0; i < tipPercentage.length; i++) {
+  tipPercentage[i].addEventListener("click", calculateTipPercentage);
 }
