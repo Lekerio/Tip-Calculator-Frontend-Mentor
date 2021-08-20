4fc03ee8fdff65b5986db7c109865078fc3d64c2
Theo Lebegue <theo.lebegue1@gmail.com>
2021-08-20 16:45:13 +0200

    responsive left

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let tipPercentage = document.getElementsByClassName("tipPercentage");
 let activeClassName = "active";
 let bill = document.getElementById("bill");
+let numberpeople = document.getElementById("numberpeople");
+
 
 function activeState(items, activeName) {
   for (let i = 0; i < items.length; i++) {
@@ -21,16 +23,30 @@ function calculateTipPercentage() {
   function retrieveTipPercentage() {
     for (let i = 0; i < tipPercentage.length; i++) {
       if (tipPercentage[i].classList.contains("active")) {
+        if (tipPercentage[i].value == 0) {
+            
+        }
         return tipPercentage[i].value;
       }
     }
   }
   let chosenTip = retrieveTipPercentage();
+  let result1 = document.getElementById("result1");
   let result2 = document.getElementById("result2");
+  let finalTip = (chosenTip / 100) * bill.value;
 
-  result2.innerText = (chosenTip / 100) * bill.value;
+  if (numberpeople.value <= 0 || bill.value <= 0) {
+    return;
+  } else {
+
+    result2.innerText = finalTip.toFixed(2);
+    result1.innerText = (finalTip / numberpeople.value).toFixed(2);
+  }
 }
 
 for (let i = 0; i < tipPercentage.length; i++) {
   tipPercentage[i].addEventListener("click", calculateTipPercentage);
 }
+
+bill.addEventListener("input", calculateTipPercentage)
+numberpeople.addEventListener("input", calculateTipPercentage)
